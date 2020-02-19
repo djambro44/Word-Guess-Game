06cb3c48147685c2e940d261d00f5638a9c54145
Dylan Ambrose <djambro44@gmail.com>
2020-02-18 18:40:36 -0700

    finished function wrong and the button to select a word

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,6 +1,8 @@
-var currentWordDiv = document.getElementById("currentWord");
+var currentWordDiv = document.getElementById("randomWord");
+var remainingGuessesDiv = document.getElementById("guessesLeft")
 var remainingGuesses = 15;
-var letterGuessed = document.getElementById("guessedLetters");
+var letterGuessedDiv = document.getElementById("guessedLetters");
+
 //wins
 //losses
 
@@ -13,51 +15,63 @@ var words = [
     "cowboy"
 ];
 
-// picks random word from words array
+// empty arrays
 var answerArray = [];
 var placeHolder = [];
 //array for function wrong
 var incorrect = [];
+var userGuess;
 
 //function to get _ for each character of var word
 function initCurrentWord() {
+    currentWordDiv.innerHTML = "";
+    placeHolder = [];
     var word = words[Math.floor(Math.random() * words.length)];
     answerArray = word.split("");
     for (var i = 0; i < word.length; i++) {
         placeHolder[i] = "_";
-        document.getElementById("randomWord").innerHTML = placeHolder;
     }
+    currentWordDiv.innerHTML = placeHolder;
+    console.log(answerArray, placeHolder);
 }
 initCurrentWord();
 
 
-console.log(answerArray, placeHolder);
 
 
 
 
 //gets user input and stores it as userGuess
 document.onkeyup = function (event) {
-    var userGuess = event.key.toLowerCase();
+    userGuess = event.key.toLowerCase();
     console.log(userGuess);
+    checkUserGuess();
 }
 
+function checkUserGuess () {
+// 1) check user guess against answerArray
+//      a) if true - update placeholder to show userGuess(right)
+//              I) if placeHolder ==== answerArray trigger Congrats
+//      b) if false - add userGuess to letterGuessedDiv(wrong)
+
+}
 
 
 
 //push wrong letter guesses 
 function wrong() {
     remainingGuesses--;
-    var remainingGuesses = document.getElementById("guessesLeft");
-    userGuess.push(incorrect);
-    var incorrect = document.getElementById("guessedLetters");
+    console.log(remainingGuesses);
+    remainingGuessesDiv.textContent = remainingGuesses; 
+    incorrect.push(userGuess);
+    letterGuessedDiv.innerHTML = incorrect;
 }
-console.log(wrong);
+wrong();
 
 //user guesses correctly
 function right() {
-    var placeHolder = []
-    var userGuess = placeHolder.join("userGuess");
+    //UserGuess append placeHolder
+    //
 }
 
 //game over function
