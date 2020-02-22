d9f55015c4afc0d2acd663465a77009cad058f02
Dylan Ambrose <djambro44@gmail.com>
2020-02-22 10:01:36 -0700

    formating, updated start game

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,6 +1,7 @@
 // make variabels
 //     userGuess 
 var userGuess;
+
 //     words - array 
 var words = [
     "scorpion",
@@ -12,23 +13,51 @@ var words = [
 ];
 //     selected word 
 var selectedWord;
+
 // split selected word
 var splitSelectedWord
+
 // selected word _'s 
 var underscores = [];
+
+//selected word div
+var selectedWordDiv = document.getElementById("randomWord");
+
 //     incorrect guess 
 var incorrectGuess;
+
 //     correct guess 
 var correctGuess;
-//     remaining guesses 
-var remainingGuesses;
+
+//     remaining guesses Div
+var remainingGuessesDiv = document.getElementById("guessesLeft");
+
+//      remaining guesses
+var remainingGuesses = 15;
+
 //     incorrect guesses 
-var incorrectGuesses;
+var incorrectGuessesDiv = document.getElementById("guessedLetters");
+
+// incorrected guesses
+var incorrectGuess = [];
+
 //     wins 
 var wins;
+
 //     losses 
 var losses;
+
 // make functions 
+
+// start game
+function startGame() {
+    remainingGuesses = 15;
+    document.getElementById("guessesLeft").textContent = remainingGuesses;
+    incorrectGuesses = [];
+    getRandomWord();
+}
+
+
 //     selecting random word from word array and  
 function getRandomWord() {
     var selectedWord = words[Math.floor(Math.random() * words.length)];
@@ -38,11 +67,14 @@ function getRandomWord() {
     console.log(splitSelectedWord);
     //     setting _ for each character in selected word
     for (i = 0; i < selectedWord.length; i++){
-        var underscores = splitSelectedWord[i] = "_";
-        console.log(underscores);
+        underscores[i] = "_";
+        selectedWordDiv.innerHTML = underscores;
     }
+    console.log(underscores);
+    selectedWordDiv.innerHTML = underscores;
 }
-getRandomWord();
+
+
 
 //     get user input 
 document.onkeyup = function (event) {
@@ -50,17 +82,34 @@ document.onkeyup = function (event) {
     console.log(userGuess);
 }
 
+// when userGuess !== splitSelectedWord
+function wrongGuess() {
+    remainingGuesses--;
+    remainingGuessesDiv.textContent = remainingGuesses;
+    incorrectGuess.push(userGuess);
+    incorrectGuessesDiv.innerHTML = incorrectGuess;
+    console.log(remainingGuesses, incorrectGuess);
+}
+
+
+
+
+
+
+
+
 //     check if user input matches currect word single character 
 if (userGuess === splitSelectedWord) {
     //         if yes - place correct guess  in place of _ 
     underscores.join(userGuess);
 } else {
-    remainingGuesses--;
+    wrong();
     //         if no - user losses a guess and incorrect guess is stored as incorrect guess 
     incorrectGuesses.join(userGuess);
 }
 
 
 //     if user guesses all correct letters - add one to wins 
+
 //     if user runs out of guesses prompt game over 
 
