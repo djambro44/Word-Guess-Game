6f1227453a69213b3b86ad77300f1d7b12a07e90
Dylan Ambrose <djambro44@gmail.com>
2020-02-27 18:49:44 -0700

    finsihed gameplay. Still need to work on lose function

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -39,7 +39,7 @@ var remainingGuesses = 15;
 var incorrectGuessesDiv = document.getElementById("guessedLetters");
 
 // incorrected guesses
-var incorrectGuess = [];
+var incorrectGuesses = [];
 
 var numBlanks = [];
 
@@ -58,13 +58,10 @@ function startGame() {
     remainingGuesses = 15;
     document.getElementById("guessesLeft").textContent = remainingGuesses;
     incorrectGuesses = [];
-    getRandomWord();
-    checkUserGuess();
+    reset()
     endOfGame();
 }
 
-
-//     selecting random word from word array and  
 function getRandomWord() {
     selectedWord = words[Math.floor(Math.random() * words.length)];
     console.log(selectedWord);
@@ -76,18 +73,25 @@ function getRandomWord() {
     blanksAndSuccesses = [];
     underscores = [];
     //     setting _ for each character in selected word
-    for (i = 0; i < selectedWord.length; i++){
+    for (i = 0; i < selectedWord.length; i++) {
         // blanksAndSuccesses.push(underscores);
         underscores[i] = "_";
         selectedWordDiv.innerHTML = underscores;
         console.log(blanksAndSuccesses)
     }
-    
+
     console.log(underscores);
     selectedWordDiv.innerHTML = underscores;
     //this is not working
     // document.getElementById("randomWord").innerHTML = blanksAndSuccesses.join(" ");
-}
+};
+function reset() {
+    getRandomWord();
+    remainingGuesses = 15;
+    document.getElementById("guessesLeft").textContent = remainingGuesses;
+    incorrectGuesses = [];
+    document.getElementById("guessedLetters").textContent = "";
+};
 
 
 
@@ -95,34 +99,43 @@ function getRandomWord() {
 document.onkeyup = function (event) {
     userGuess = event.key.toLowerCase();
     console.log(userGuess);
-    checkUserGuess();
-}
-
-function checkUserGuess (userGuess){
-    // for (i = 0; i < selectedWord; i++) {
-        if (userGuess === splitSelectedWord){
-            document.getElementById("randomWord").innerHTML = userGuess;
-            console.log(selectedWord);
-        } else {
-            // document.getElementById("guessedLetters").innerHTML = userGuess;
+    console.log(selectedWord.includes(userGuess));
+    if (!selectedWord.includes(userGuess)) {
+        incorrectGuess = userGuess;
         wrongGuess();
+    } else {
+        for (var i = 0; i < selectedWord.length; i++) {
+            if (userGuess === splitSelectedWord[i]) {
+                underscores[i] = userGuess;
+                selectedWordDiv.innerHTML = underscores;
+            }
+        } 
+            if (underscores.join() === splitSelectedWord.join()){
+                alert("Congrats! You win!");
+                reset();
+            } 
+            
         }
-    // }
+                if (remainingGuesses === "0") {
+                    alert("You lose!");
+                    reset();
+                }
 };
+
 
 // when userGuess !== splitSelectedWord
 function wrongGuess() {
     remainingGuesses--;
     remainingGuessesDiv.textContent = remainingGuesses;
-    incorrectGuess.push(userGuess);
-    incorrectGuessesDiv.innerHTML = incorrectGuess;
+    incorrectGuesses.push(incorrectGuess);
+    incorrectGuessesDiv.innerHTML = incorrectGuesses;
     console.log(remainingGuesses, incorrectGuess);
 };
 
 function endOfGame() {
-   if (remainingGuesses === 0) {
-       alert("Game Over!");
-   };
+    if (remainingGuesses === 0) {
+        alert("Game Over!");
+    };
 };
 
 
@@ -131,15 +144,6 @@ function endOfGame() {
 
 
 
-//     check if user input matches currect word single character 
-if (userGuess === splitSelectedWord) {
-    //         if yes - place correct guess  in place of _ 
-    underscores.join(userGuess);
-} else {
-    wrong();
-    //         if no - user losses a guess and incorrect guess is stored as incorrect guess 
-    incorrectGuesses.join(userGuess);
-}
 
 
 //     if user guesses all correct letters - add one to wins 
